ef48ab96ce991964b9c6ee95ea494ad3b362b8f4
Alan-Gomes1 <alan.gomes.ag28@gmail.com>
2023-06-03 22:45:08 -0300

    Criando o app cliente

---
+++ cliente/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path(
+        'meus_certificados/', views.meus_certificados,
+        name='meus_certificados'
+    ),
+]
